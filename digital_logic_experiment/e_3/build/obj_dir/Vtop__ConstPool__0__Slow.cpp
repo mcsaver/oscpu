858fd3cf0dc6cb466c80707051f40d4a4710fcbc
26010035-Yigeng Li <1727935578@qq.com>
2026-08-02 20:58:16 +0800

    before linux poweroff

--- digital_logic_experiment/e_3/build/obj_dir/Vtop__ConstPool__0__Slow.cpp
+++
@@ -1,8 +0,0 @@
-// Verilated -*- C++ -*-
-// DESCRIPTION: Verilator output: Constant pool
-
-#include "verilated.h"
-
-extern const VlUnpacked<CData/*3:0*/, 8> Vtop__ConstPool__TABLE_h97d80722_0 = {{
-    7U, 0x0bU, 0x0eU, 0U, 7U, 7U, 1U, 0U
-}};
